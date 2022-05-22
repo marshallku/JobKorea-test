2baf620f3674db89ab33b7ad344c02a767fbfcc0
marshallku <marshall@kakao.com>
2022-05-22 17:00:34 +0900

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 export default function App() {
-    return <div>Hello World!</div>;
+    return (
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <nav className="links">
+                <a className="links__link" href="/sign-up">
+                    Sign Up
+                </a>
+                <a className="links__link" href="/list">
+                    List
+                </a>
+            </nav>
+        </div>
+    );
+}

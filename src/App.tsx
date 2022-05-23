b43ce6494bf5ff4f2b4f4851b69a6047de54965a
marshallku <marshall@kakao.com>
2022-05-23 16:21:11 +0900

    Add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import List from "./pages/List";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
     return (
         <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
-                <Route path="/" element={<Home />}></Route>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/list" element={<List />} />
             </Routes>
         </BrowserRouter>
     );

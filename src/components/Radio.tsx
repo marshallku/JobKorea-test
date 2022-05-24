365203b62ebec09ae8ca97789d6b055859fc15a0
marshallku <marshall@kakao.com>
2022-05-24 17:45:07 +0900

    Update design of radio

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,16 +1,22 @@
+import "./Radio.css";
+
 export default function Radio({ items, groupName, stateSetter }: RadioProps) {
     return (
         <div>
             {items.map(({ id, name }) => (
-                <span key={id}>
+                <span key={id} className="radio">
                     <input
                         type="radio"
                         name={groupName}
                         id={`${groupName}-${id}`}
+                        className="radio__input"
                         onChange={() => {
                             stateSetter(name);
                         }}
                     />
+                    <div className="radio__icon">
+                        <div className="radio__circle" />
+                    </div>
                     <label htmlFor={`${groupName}-${id}`}>{name}</label>
                 </span>
             ))}

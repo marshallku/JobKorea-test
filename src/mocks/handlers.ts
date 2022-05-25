a121ee166d855b10d4f2c14a5a0c1a462e70e22f
marshallku <marshall@kakao.com>
2022-05-25 19:29:58 +0900

    Add mock data

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,11 +3,40 @@ import { rest } from "msw";
 const BASE_URL = `${import.meta.env.BASE_URL}api/`;
 
 const handlers = [
-    rest.get(`${BASE_URL}user`, (_, res, ctx) => {
+    rest.get(`${BASE_URL}interview-review`, (req, res, ctx) => {
+        const getPassed = () => {
+            const passed = req.url.searchParams.get("passed");
+
+            if (typeof passed === "string") {
+                return +passed === 1;
+            }
+
+            return Math.random() < 0.5;
+        };
+
         return res(
             ctx.status(200),
             ctx.json({
-                username: "admin",
+                success: true,
+                data: {
+                    count: 3512,
+                    items: [...Array(20)].map(() => ({
+                        company: "Company Name",
+                        userName: "John Doe",
+                        date: new Date(
+                            2022,
+                            Math.floor(Math.random() * 12),
+                            Math.floor(Math.random() * 31)
+                        ),
+                        view: Math.floor(Math.random() * 300),
+                        comments: Math.floor(Math.random() * 10),
+                        passed: getPassed(),
+                        responsible: "Developer",
+                        interviewAt: new Date(),
+                        description:
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis elementum nunc nec pretium. Maecenas quam purus, venenatis non mi vitae, euismod ultrices tellus. Morbi nec est eget turpis gravida rhoncus. Etiam vitae facilisis nisi, eget bibendum ipsum. Donec rutrum rhoncus nunc ac lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus risus lacus, posuere et aliquam in, vulputate tincidunt elit. Morbi eu dignissim quam, maximus eleifend erat.",
+                    })),
+                },
             })
         );
     }),

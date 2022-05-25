7c1f5d43ddc9de37b1e9d9f36896b235edd4e13f
marshall-ku <marshall@kakao.com>
2022-05-25 22:16:41 +0900

    Fix service worker path

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import worker from "./mocks/browser";
 import "./index.css";
 
 // FIXME: Worker starts even import.meta.env.DEV is false
-worker.start();
+worker.start({
+    serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js` },
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />

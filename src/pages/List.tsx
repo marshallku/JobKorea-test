969ea9bddfa99e8f9fb9dead8cc4aeb4828e631f
marshallku <marshall@kakao.com>
2022-05-25 20:42:45 +0900

    Implement search filter

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -8,6 +8,8 @@ export default function List() {
         count: number;
         items: InterviewReviewItem[];
     }>();
+    const [tmpFilterOption, setTmpFilterOption] =
+        useState<InterviewReviewProps>({});
     const [filterOption, setFilterOption] = useState<InterviewReviewProps>({});
     const fetchList = useCallback(async () => {
         const { data, success } = await getInterviewReview(filterOption);
@@ -18,6 +20,12 @@ export default function List() {
 
         setData(data);
     }, [filterOption]);
+    const setPassed = useCallback(
+        (passed?: 0 | 1) => {
+            setTmpFilterOption((x) => ({ ...x, passed }));
+        },
+        [setTmpFilterOption]
+    );
 
     useEffect(() => {
         fetchList();
@@ -29,9 +37,56 @@ export default function List() {
 
     return (
         <div className="list">
-            {data.items.map((x) => (
-                <InterviewReviewItem key={`${x.id}`} data={x} />
-            ))}
+            <div className="list__option">
+                <div>
+                    총 <span className="highlight">{data.count}</span>건
+                </div>
+            </div>
+            <div className="list__items">
+                {data.items.map((x) => (
+                    <InterviewReviewItem key={`${x.id}`} data={x} />
+                ))}
+            </div>
+            <div className="list__drawer drawer">
+                <h2>합격 여부</h2>
+                <div className="drawer__buttons">
+                    <button
+                        type="button"
+                        onClick={() => {
+                            setPassed(undefined);
+                        }}
+                    >
+                        전체
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => {
+                            setPassed(1);
+                        }}
+                    >
+                        합격
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => {
+                            setPassed(0);
+                        }}
+                    >
+                        불합격
+                    </button>
+                </div>
+                <div className="drawer__center">
+                    <button
+                        type="button"
+                        className="drawer__submit"
+                        onClick={() => {
+                            setFilterOption({ ...tmpFilterOption });
+                        }}
+                    >
+                        검색
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
